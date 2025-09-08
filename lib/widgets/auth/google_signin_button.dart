adc3869672f1ba679e5b5b7d35f05a5c6d13b6c3
Kqn0n <taharacomeon.0504@gmail.com>
2025-09-08 17:24:18 +0900

    fix  : 構文エラーの修正 - google_signin_button.dart - web/index.html

--- lib/widgets/auth/google_signin_button.dart
+++ lib/widgets/auth/google_signin_button.dart
@@ -25,7 +25,7 @@ class GoogleSignInButton extends StatelessWidget {
         border: Border.all(color: Colors.grey[300]!),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.1),
+            color: Colors.black.withValues(alpha: 0.1),
             blurRadius: 2,
             offset: const Offset(0, 1),
           ),
